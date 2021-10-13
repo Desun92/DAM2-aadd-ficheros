16e6a32c4e60fe395a0bf201edb784b36a710423
alumnoA <alumnoA@SMR2B011>
2021-10-13 12:02:43 +0200

    A implementar XML

--- Practicas Ficheros/src/es/iestetuan/acv/GestionDirectoriosFicheros.java
+++ Practicas Ficheros/src/es/iestetuan/acv/GestionDirectoriosFicheros.java
@@ -1,23 +1,68 @@
 package es.iestetuan.acv;
 
-import java.io.File;
-import es.iestetuan.acv.directorios_ficheros.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import es.iestetuan.acv.directorios_ficheros.CreacionDirectoriosFicheros;
 
 public class GestionDirectoriosFicheros {
 	
 	public static void main(String[] args) {
+
+		Properties confi = new Properties();
+		InputStream in = null;
+		CreacionDirectoriosFicheros directorio = new CreacionDirectoriosFicheros();
+		JsonObject propiedadesJson = null;
 		
-		String ruta1="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\00-tmp\\Texto-ACV";
-		String ruta2="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\00-tmp\\XML-ACV";
-		String ruta3="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\Texto\\backup";
-		String ruta4="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\Texto\\info\\alumno";
-		String ruta5="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\Texto\\procesados";
-		String ruta6="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\XML\\backup";
-		String ruta7="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\XML\\info\\alumno";
-		String ruta8="C:\\Users\\alumnoA\\Desktop\\AccesoDatos\\Ficheros\\XML\\procesados";
+		try {
+			BufferedReader leer =new BufferedReader(new FileReader("C:\\Users\\alumnoA\\git\\repository2(practicas_ficheros)\\Practicas Ficheros\\recursos\\info-configuracion.json"));
+			String json="";
+			String linea="";
+			
+			while((linea=leer.readLine())!=null){
+				json+=linea;
+			}
+			
+			propiedadesJson = JsonParser.parseString(json).getAsJsonObject();
+			
+			
+			//Implementacion de properties
+			//in = new FileInputStream("D:\\Clase\\Repositorios\\Eclipse\\DAM2-aadd-ficheros\\Practicas Ficheros\\recursos\\info-configuracion.properties");
+			//confi.load(in);
+		}
+		catch(IOException ioex) {
+			System.err.println("No se ha podido encontrar el archivo de configuraci�n");
+			ioex.printStackTrace();
+		}
 		
+		//Creacion directorios
+		for(int i=1;i<=8;i++) {
+			//Properties
+			//directorio.crearDirectorio(confi.getProperty("ruta"+i));
+			//JSON
+			//directorio.crearDirectorio(propiedadesJson.get("ruta"+i).getAsString());
+		}
 		
+		//Creacion ficheros vacios
+		//Properties
+		//directorio.crearFicheroNombre(confi.getProperty("ruta0"), "info-temporal.txt");
+		//JSON
+		//directorio.crearFicheroNombre(propiedadesJson.get("ruta0").getAsString(), "info-temporal.txt");
 		
-	}
+		//Borrado ficheros
+		//Properties
+		//directorio.borrar(confi.getProperty("ruta0"));
+		//JSON
+		//directorio.borrar(propiedadesJson.get("ruta0").getAsString());
+	
+}
 	
 }
